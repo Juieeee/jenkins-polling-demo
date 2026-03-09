ffba71f2154648c6d01f7efaba699b35d95f37dd
Jui V <157942333+Juieeee@users.noreply.github.com>
2026-03-09 23:21:26 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub Repository");
-        System.out.println("Build triggered by poll SCM");
+        System.out.println("Build triggered by poll SCM....");
     }
 }
