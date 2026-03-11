854381a12879662a76b1734080e1bbb1bef5e77b
Jui V <157942333+Juieeee@users.noreply.github.com>
2026-03-11 19:30:45 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from GitHub Repository");
-        System.out.println("Build triggered by poll SCM....");
-        System.out.println("Hello this file is updated");
+        System.out.println("Hello Jenkins Pipeline");
     }
 }
