cce8ca593e481bac53d6442050c9f70cafa38288
Jui V <157942333+Juieeee@users.noreply.github.com>
2026-03-11 19:08:17 +0530

    Fix output message in Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub Repository");
         System.out.println("Build triggered by poll SCM....");
-        System.out.println("Hello this file is updated!!");
+        System.out.println("Hello this file is updated");
     }
 }
